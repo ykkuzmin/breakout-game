8095da0c0408e6b55d1e6ae2e6c78ff1488fe2b8
Yarik Kuzmin <ykkuzmin@gmail.com>
2025-06-02 18:50:10 -0400

    added reflections, game over

--- src/main/java/com/example/breakout/CallingNames.java
+++ src/main/java/com/example/breakout/CallingNames.java
@@ -4,9 +4,15 @@ public class CallingNames {
 	
 	public static String response(String name, String difficulty) {
         String txt = "";
-		if (difficulty.equals("Easy")) {
+		if (difficulty.equals("Easy")) 
+		{
         	txt = "You are a chicken!";
-        } else if (difficulty.equals("Masochist")) {
+        } else if (difficulty.equals("Masochist")) 
+        {
+        	txt = "You must be mental!";
+        }
+        else if (difficulty.equals("Masochist")) 
+        {
         	txt = "You must be mental!";
         }
 		txt = "Welcome " + name + "! You selected " + difficulty + " difficulty. " + txt;
